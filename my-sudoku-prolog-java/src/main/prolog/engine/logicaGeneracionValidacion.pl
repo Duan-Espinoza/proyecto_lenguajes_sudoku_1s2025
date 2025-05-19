492a0c1fcfe549e124904c633d3d14709af35e33
Duan-Espinoza <75913012+Duan-Espinoza@users.noreply.github.com>
2025-05-19 16:24:57 -0600

    Update logicaGeneracionValidacion.pl

---
+++ my-sudoku-prolog-java/src/main/prolog/engine/logicaGeneracionValidacion.pl
@@ -0,0 +1,3 @@
+% Acá se encargará mediante este archivo o la combinación de varios
+% de la generación de una matriz, la cual esta será la que represente el juego en
+% su totalidad, esta tendrá sus validaciones respectivas
